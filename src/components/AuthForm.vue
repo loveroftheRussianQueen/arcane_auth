47c943b13105f6139ec1664ec1d5a2ee08537014
Your Name <al1m.budaev@yandex.ru>
2023-07-17 16:27:51 +0300

    feat: add second step of verification

--- src/components/AuthForm.vue
+++ src/components/AuthForm.vue
@@ -6,46 +6,71 @@
           <div class="container__main__form">
             <div class="container__main__form__header">
               <div class="logo">Логотип (Высота 40px, длина до 300px)</div>
-              <h2 class="title">Введите номер телефона</h2>
-              <p>Чтобы войти или зарегистрироваться</p>
+              <h2 class="title" v-if="step === 'create'">Введите номер телефона</h2>
+              <h2 class="title" v-if="step === 'send'">Введите код</h2>
+              <p v-if="step === 'create'">Чтобы войти или зарегистрироваться</p>
+              <p v-if="step === 'send'">Отправлен по номеру {{ phone }}</p>
             </div>
             <div class="container__main__form__inputs">
               <div class="country">
-                <label>Страна</label>
-                <select v-model="selected" :value="selected" @change="changeCode" placeholder="Страна">
-                  <input typ="search" placeholder="Впишите страну"/>
+                <label v-if="step==='create'">Страна</label>
+                <select
+                  v-model="selected"
+                  :value="selected"
+                  @change="changeCode"
+                  placeholder="Страна"
+                  v-if="step === 'create'"
+                >
                   <option v-for="code in codes" v-bind:value="code.dial_code">
                     {{ code.name }}
                     {{ code.flag }}
                     {{ code.dial_code }}
                   </option>
                 </select>
+                <label v-if="step==='send'">Способ получения кода</label>
+                <select
+                  v-model="option"
+                  :value="option"
+                  placeholder="Способ получения кода"
+                  v-if="step === 'send'"
+                >
+                  <option v-for="option in options" v-bind:value="option.name">
+                    {{ option.name }}
+                    <img :src="option.icon" alt="Social media icon"/>
+                  </option>
+                </select>
               </div>
               <div class="number">
                 <label>Номер телефона</label>
-                <label>Номер телефона</label>
-                <input type="text" placeholder="Введите код" v-model="code" v-if="step == 'send'"/>
-                <input :class="{invalid: !isValidPhoneNumber}" type="text" placeholder="Номер телефона" v-model="phone"  @keyup="validatePhoneNumber"/>
+                <input type="text" placeholder="Введите код" v-model="code" v-if="step == 'send'" />
+                <input
+                  :class="{ invalid: !isValidPhoneNumber }"
+                  type="text"
+                  placeholder="Номер телефона"
+                  v-model="phone"
+                  @keyup="validatePhoneNumber"
+                  v-if="step === 'create'"
+                />
                 <div class="validation" v-if="!isValidPhoneNumber">Неверный формат номера</div>
               </div>
             </div>
           </div>
           <div class="container__main__button">
-            <button @click.prevent="createSession">Продолжить</button>
-            <button @click.prevent="createSession">Продолжить</button>
+            <button @click.prevent="createSession" v-if="step === 'create'">Продолжить</button>
+            <button @click.prevent="sendCode" v-if="step === 'send'">Отправить</button>
           </div>
         </div>
         <div class="container__footer">
-            <div class="container__footer__language">
-                <select v-model="lang">
-                    <option>Русский</option>
-                    <option>English</option>
-                </select>
-            </div>
-            <div class="container__footer__terms">
-                <p>Условия</p>
-                <p>Конфиденциальность</p>
-            </div>
+          <div class="container__footer__language">
+            <select v-model="lang">
+              <option>Русский</option>
+              <option>English</option>
+            </select>
+          </div>
+          <div class="container__footer__terms">
+            <p>Условия</p>
+            <p>Конфиденциальность</p>
+          </div>
         </div>
       </div>
     </form>
@@ -54,60 +79,85 @@
 
 <script setup>
 import { ref, onUpdated, onMounted } from 'vue'
-import axios from 'axios';
-import codes from '../api/data/phone_codes.json';
+import axios from 'axios'
+import codes from '../api/data/phone_codes.json'
+import whatsapp from '../assets/icons/whatsapp.svg'
 
-const selected = ref(null);
-const lang = ref('Русский');
-const phone = ref('');
-let isValidPhoneNumber = ref(true);
-const step = ref('');
-const session_id = ref('');
-const options = ref(['list', 'of', 'options']);
+const selected = ref(null)
+const lang = ref('Русский')
+const phone = ref('')
+let isValidPhoneNumber = ref(true)
+const step = ref('create')
+const code = ref('')
+const session_id = ref('')
+const options = ref([
+  {
+    name: 'Whatsapp',
+    icon: '/src/assets/icons/whatsapp.svg'
+  },
+  {
+    name: 'Telegram',
+    icon: ''
+  },
+  {
+    name: 'Viber',
+    icon: ''
+  },
+  {
+    name: 'SMS',
+    icon: ''
+  }
+])
+const option = ref('')
 
-onMounted(() =>{
-  console.log(codes);
+onMounted(() => {
+  console.log(codes)
 })
 
-const key = import.meta.env.VITE_API_KEY;
+const key = import.meta.env.VITE_API_KEY
 
-const validatePhoneNumber = () =>{
-  const validationRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+const validatePhoneNumber = () => {
+  const validationRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
   if (phone.value.match(validationRegex)) {
-        isValidPhoneNumber.value = true;
-      } else {
-        isValidPhoneNumber.value = false;
-      }
-}
-
-const createSession = async() =>{
-  if(isValidPhoneNumber.value == true){
-    phone.value = phone.value.replaceAll('+', '');
-    const response = await axios.get(`https://api.dev.kod.mobi/api/v1/message/create?phone=${phone.value}&type=sms&lang=ru&x-api-key=${key}`);
-    session_id.value = response.data.data.session_id;
-    step.value = 'create';
-    console.log(session_id.value);
-  }else{
-    console.log('Invalid phone number');
-    console.log(isValidPhoneNumber.value);
+    isValidPhoneNumber.value = true
+  } else {
+    isValidPhoneNumber.value = false
   }
 }
 
-const sendCode = async() =>{
-  if(session_id.value){
-    const response = axios.get(`https://api.dev.kod.mobi/api/v1/message/send?session_id=&type=sms&x-api-key=${key}`)
+const createSession = async () => {
+  if (isValidPhoneNumber.value == true) {
+    phone.value = phone.value.replaceAll('+', '')
+    const response = await axios.get(
+      `https://api.dev.kod.mobi/api/v1/message/create?phone=${phone.value}&type=telegram&lang=ru&x-api-key=${key}`
+    )
+    session_id.value = response.data.data.session_id
+    step.value = 'send'
+    console.log(response.data)
+  } else {
+    console.log('Invalid phone number')
+    console.log(isValidPhoneNumber.value)
   }
 }
-onUpdated(() =>{
-  console.log(isValidPhoneNumber.value);
+
+const sendCode = async () => {
+  if (session_id.value) {
+    const response = await axios.get(
+      `https://api.dev.kod.mobi/api/v1/message/send?session_id=${session_id.value}&type=sms&x-api-key=${key}`
+    )
+    console.log(response.data)
+  }
+}
+onUpdated(() => {
+  console.log(isValidPhoneNumber.value)
 })
-const changeCode = () =>{
-  phone.value = selected.value;
+const changeCode = () => {
+  phone.value = selected.value
 }
-
 </script>
 
 <style lang="scss" scoped>
+@import 'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500';
 .main {
   display: flex;
   justify-content: center;
@@ -211,16 +261,16 @@ const changeCode = () =>{
             font-size: 16px;
             font-style: normal;
             font-weight: 400;
-            line-height: 24px; 
-            letter-spacing: 0.15px; 
+            line-height: 24px;
+            letter-spacing: 0.15px;
             -webkit-appearance: none;
             -moz-appearance: none;
             appearance: none;
-            background: url('../assets/icons/arrow.svg') no-repeat  96% / 15% #fff;
+            background: url('../assets/icons/arrow.svg') no-repeat 96% / 15% #fff;
             background-size: 25px;
             border-radius: 4px;
 
-            &:focus{
+            &:focus {
               outline: none;
             }
           }
@@ -248,32 +298,32 @@ const changeCode = () =>{
             border: 1px solid rgba(0, 0, 0, 0.12);
             border-radius: 4px;
 
-            &::placeholder{
-                font-family: Roboto;
-                font-size: 16px;
-                font-style: normal;
-                font-weight: 400;
-                line-height: 24px; 
-                letter-spacing: 0.15px; 
+            &::placeholder {
+              font-family: Roboto;
+              font-size: 16px;
+              font-style: normal;
+              font-weight: 400;
+              line-height: 24px;
+              letter-spacing: 0.15px;
             }
 
-            &:focus{
+            &:focus {
               outline: none;
             }
 
-            &.invalid{
-              border: 1px solid #EB4036;
+            &.invalid {
+              border: 1px solid #eb4036;
             }
           }
 
-          .validation{
+          .validation {
             font-family: Roboto;
             font-size: 12px;
             font-style: normal;
             font-weight: 400;
             line-height: 16px; /* 133.333% */
-            letter-spacing: 0.4px; 
-            color: #EB4036;
+            letter-spacing: 0.4px;
+            color: #eb4036;
             margin-left: 16px;
             margin-top: 4px;
           }
@@ -281,84 +331,84 @@ const changeCode = () =>{
       }
     }
 
-    &__button{
+    &__button {
+      display: flex;
+      padding: 0px 32px;
+      justify-content: flex-end;
+      align-items: center;
+      gap: 10px;
+      align-self: stretch;
+      width: 100%;
+
+      button {
+        cursor: pointer;
         display: flex;
-        padding: 0px 32px;
-        justify-content: flex-end;
+        height: 55px;
+        padding: 0px 16px;
+        justify-content: center;
         align-items: center;
-        gap: 10px;
-        align-self: stretch; 
-        width: 100%;
-        
-        button{
-            cursor: pointer;
-            display: flex;
-            height: 55px;
-            padding: 0px 16px;
-            justify-content: center;
-            align-items: center;
-            gap: 16px;
-            flex: 1 0 0; 
-            border-radius: 4px; 
-            border: 0px;
-            background: #007AFF;
-            color: #FFF;
-            text-align: center;
-            font-family: Roboto;
-            font-size: 16px;
-            font-style: normal;
-            font-weight: 500;
-            line-height: 36px; 
-        }
+        gap: 16px;
+        flex: 1 0 0;
+        border-radius: 4px;
+        border: 0px;
+        background: #007aff;
+        color: #fff;
+        text-align: center;
+        font-family: Roboto;
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 500;
+        line-height: 36px;
+      }
     }
   }
 
-  &__footer{
+  &__footer {
     display: flex;
     padding: 0px 32px 24px;
     justify-content: space-between;
     align-items: flex-start;
-    align-self: stretch; 
+    align-self: stretch;
     width: 100%;
 
-    &__language{
-        select{
-            cursor: pointer;
-            color: #666;
-            font-family: Roboto;
-            font-size: 12px;
-            font-style: normal;
-            font-weight: 400;
-            line-height: 24px;
-            -webkit-appearance: none;
-            -moz-appearance: none;
-            appearance: none;
-            background: url('../assets/icons/arrow_small.svg') no-repeat 65% transparent;
-            background-size: 14px;
-            border-radius: 4px;
-            border: 0px;
-            width: 100px;
+    &__language {
+      select {
+        cursor: pointer;
+        color: #666;
+        font-family: Roboto;
+        font-size: 12px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 24px;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+        background: url('../assets/icons/arrow_small.svg') no-repeat 65% transparent;
+        background-size: 14px;
+        border-radius: 4px;
+        border: 0px;
+        width: 100px;
 
-            &:focus{
-              outline: none;
-            }
+        &:focus {
+          outline: none;
         }
+      }
     }
 
-    &__terms{
-            display: flex;
-            gap: 24px;
+    &__terms {
+      display: flex;
+      gap: 24px;
 
-            p{
-                cursor: pointer;
-                color: #666;
-                font-family: Roboto;
-                font-size: 12px;
-                font-style: normal;
-                font-weight: 400;
-                line-height: 24px; /* 200% */ 
-            }
-        }
+      p {
+        cursor: pointer;
+        color: #666;
+        font-family: Roboto;
+        font-size: 12px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 24px; /* 200% */
+      }
+    }
   }
 }
 </style>
